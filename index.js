b559136020ab554c3d0a8b1e2c66f890ac5baed8
raghu-0232 <58412332+raghu-0232@users.noreply.github.com>
2022-02-23 13:17:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 var app = express()
 app.get("/",function(request,response){
 response.send("Hello World!")
+  process.exit()
 })
 app.listen(10000, function () {
 console.log("Started application on port %d", 10000)
